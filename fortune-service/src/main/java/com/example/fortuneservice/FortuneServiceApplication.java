a48935e456f9295598c7d7aef1266c4c35f25b3e
mmohamedaadel <mmohamedaadel@gmail.com>
2019-02-12 14:35:33 -0600

    Add files via upload

---
+++ fortune-service/src/main/java/com/example/fortuneservice/FortuneServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example.fortuneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FortuneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortuneServiceApplication.class, args);
+	}
+
+}
+
---
+++ fortune-service/src/main/java/com/example/fortuneservice/fortune/FortuneController.java
@@ -0,0 +1,31 @@
+package com.example.fortuneservice.fortune;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class FortuneController {
+
+    private Logger logger= LoggerFactory.getLogger(getClass());
+            private FortuneService fortuneService;
+
+
+    @Autowired
+    public FortuneController(FortuneService fortuneService){
+
+        this.fortuneService=fortuneService;
+    }
+
+
+    @RequestMapping("/")
+    public String getQuote(){
+
+        logger.debug("Fetching Fortune");
+        return  fortuneService.getFortune();
+    }
+}
